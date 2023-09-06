6b093eff9d17f8374027f8789711f7f0cd7b113c
quliyevnamiq8@gmail.com <quliyevnamiq8@gmail.com>
2023-09-07 00:17:31 +0400

    ....cloudinary

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UploadedFiles, UseInterceptors, UsePipes, ValidationPipe } from '@nestjs/common';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import upload from 'src/config/multer';
+import { createHotelDto } from 'src/hotel/createhotel.dto';
+import { AdminService } from './admin.service';
 
 @Controller('admin')
-export class AdminController {}
+export class AdminController {
+  constructor(private readonly adminService: AdminService) { }
+
+  @Post('create-hotel')
+  @UsePipes(new ValidationPipe())
+  @UseInterceptors(FilesInterceptor('photos'))
+  async createHotel(@Body() CreateHotelDto: createHotelDto, @UploadedFiles() files: Array<Express.Multer.File>) {
+    const hotel = await this.adminService.createHotel(CreateHotelDto)
+
+    return hotel
+  }
+
+
+
+}
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,23 +1,23 @@
-import { HttpException, HttpStatus, Injectable, Req } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { createHotelDto } from 'src/hotel/createhotel.dto';
 import { Hotel } from 'src/hotel/hotel.schema';
 import cloudinary from 'src/config/cloudinary';
 
+
 @Injectable()
 export class AdminService {
-  constructor(@InjectModel('hotel') private hotelModel: Model<Hotel>) {}
+  constructor(@InjectModel('hotel') private hotelModel: Model<Hotel>) { }
 
   // create hotel
-  async createHotel(CreateHotelDto: createHotelDto): Promise<Hotel> {
-    const hotelExist=await this.hotelModel.findOne({name:CreateHotelDto.name})
-    if(hotelExist){
-        throw new HttpException('The hotel has already been created', HttpStatus.CONFLICT)
+  async createHotel(CreateHotelDto: createHotelDto, files: any): Promise<Hotel> {
+    const hotelExist = await this.hotelModel.findOne({ name: CreateHotelDto.name })
+    if (hotelExist) {
+      throw new HttpException('The hotel has already been created', HttpStatus.CONFLICT)
     }
-
-    const hotel =await this.hotelModel.create(CreateHotelDto)
+    const data = await cloudinary.uploader.upload(files.path, { public_id: files.originalname })
+    const hotel = await this.hotelModel.create({ photos: data.url, ...CreateHotelDto })
     return hotel
-
   }
 }
